39856b978f30da9ead3ee58969f3ca059ae684c6
Rawan-7ossam <rawanhossam4000@gmail.com>
2025-07-03 04:40:41 +0300

    push the project

---
+++ js/main.js
@@ -0,0 +1,103 @@
+const API_KEY = "89819d23816b4237950201930250107";
+
+var searchInput = document.querySelector("#searchInput");
+var findButton = document.querySelector("#findButton");
+var colTodayDegree = document.querySelector("#col-today-degree");
+
+var todayDayElement = document.querySelector("#todayDay");
+var todayDateElement = document.querySelector("#todayDate");
+var locationElement = document.querySelector("#location");
+var todayDegree = document.querySelector("#degree");
+var iconTodayDegree = document.querySelector("#iconTodayDegree");
+var textTodayDegree = document.querySelector(".text-degree");
+
+
+var tomorrowDay = document.querySelector("#tomorrowDay");
+var iconTomorrowDegree = document.querySelector("#iconTomorrowDegree");
+var tomorrowMaxDegree =document.querySelector("#tomorrowMaxDegree");
+var tomorrowMinDegree = document.querySelector("tomorrowMinDegree");
+var textTomorrowDegree = document.querySelector("#textTomorrowDegree");
+
+
+var fterTomorrowDay = document.querySelector("#afterTomorrowDay");
+var iconAfterTomorrowDegree = document.querySelector("#iconAfterTomorrowDegree");
+var afterTomorrowMaxDegree =document.querySelector("#afterTomorrowMaxDegree");
+var afterTomorrowMinDegree = document.querySelector("afterTomorrowMinDegree");
+var textAfterTomorrowDegree = document.querySelector("#textAfterTomorrowDegree");
+
+
+function getWeather(location){
+    var httpReq = new XMLHttpRequest();
+
+    const apiUrl = `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${location}&days=3`;
+
+    httpReq.open("GET", apiUrl, true);
+    httpReq.responseType = "json";
+    httpReq.send();
+
+    httpReq.addEventListener("load" , function(){
+        console.log(httpReq.response);
+        displayWeather(httpReq.response);
+        
+    })
+
+    httpReq.addEventListener("error", function() {
+        console.error(" error occurred while trying to get weather data.");
+        
+    });
+
+}
+
+
+
+function displayWeather(dataWeather){
+    var currentLocation = dataWeather.location;
+    var currentCondition = dataWeather.current;
+
+    locationElement.textContent = currentLocation.name;
+    todayDegree.textContent = `${currentCondition.temp_c}°C`;
+    iconTodayDegree.src = currentCondition.condition.icon;
+    textTodayDegree.textContent = currentCondition.condition.text;
+    
+    var forecastDays = dataWeather.forecast.forecastday;
+
+    if(forecastDays[1]){
+        var tomorrowDataWeather = forecastDays[1];
+        iconTomorrowDegree.src = tomorrowDataWeather.day.condition.icon;
+        tomorrowMaxDegree.textContent = `${tomorrowDataWeather.day.maxtemp_c}°C`;
+        tomorrowMinDegree.textContent = `${tomorrowDataWeather.day.mintemp_c}°C`;
+        textTomorrowDegree.textContent = tomorrowDataWeather.day.condition.text;
+
+    }
+
+    if(forecastDays[2]){
+        var afterTomorrowDataWeather = forecastDays[2];
+        iconAfterTomorrowDegree.src = afterTomorrowDataWeather.day.condition.icon;
+        afterTomorrowMaxDegree.textContent = `${afterTomorrowDataWeather.day.maxtemp_c}°C`;
+        afterTomorrowMinDegreetomorrowMinDegree.textContent = `${afterTomorrowDataWeather.day.mintemp_c}°C`;
+        textAfterTomorrowDegree.textContent = afterTomorrowDataWeather.day.condition.text;
+
+    }
+}
+
+findButton.addEventListener("click", function() {
+    var location = searchInput.value;
+    getWeather(location);
+});
+
+searchInput.addEventListener("input", function() {
+    var location = searchInput.value;
+    getWeather(location);
+});
+
+document.addEventListener('DOMContentLoaded', function() { //show cairo as default city
+    getWeather("Cairo"); 
+});
+
+
+
+
+
+
+
+
